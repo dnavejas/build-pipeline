bfa4b208255fe7bfbf6cc98269727c4aa2b1118a
Danny Navejas <dannynavejas@gmail.com>
2019-07-11 21:32:42 -0500

    changed destination folders for sass, autoprefixer, and new uncss. added new uncss task to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ gulp.task("sass", function() {
   return gulp
     .src("static/scss/styles.scss")
     .pipe(sass())
-    .pipe(gulp.dest("build/css"));
+    .pipe(gulp.dest("build/css/sass"));
 });
 
 gulp.task("autoprefixer", function() {
@@ -14,9 +14,23 @@ gulp.task("autoprefixer", function() {
   let autoprefixer = require("autoprefixer");
 
   return gulp
-    .src("build/css/styles.css")
+    .src("build/css/sass/styles.css")
     .pipe(sourcemaps.init())
     .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("build/css/auto"));
+});
+gulp.task("uncss", function() {
+  let unCss = require("gulp-uncss");
+  let sourcemaps = require("gulp-sourcemaps");
+  return gulp
+    .src("build/css/auto/styles.css")
+    .pipe(sourcemaps.init())
+    .pipe(
+      unCss({
+        html: ["static/html/index.html"]
+      })
+    )
     .pipe(sourcemaps.write("."))
     .pipe(gulp.dest("dist/css"));
 });
